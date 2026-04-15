967608aaba61311066f1036f097e1ad746fec433
ZhevairovskiBojan <bojanzevairovskijs@gmail.com>
2026-04-15 22:20:40 +0200

    Initial commit: Container scroll animation demo

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { HeroScrollDemo } from "@/components/demo";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white dark:bg-black">
+      <HeroScrollDemo />
+    </main>
+  );
+}
